853119eebbcb890107c6fee1142254d434ea4cd2
chenxin.dong <chenxin.dong@datayes.com>
2017-10-21 21:53:02 +0800

    添加404控制

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
+from flask import render_template
 
-
-# app = Flask(__name__)
-# app.debub = True
+app = Flask(__name__)
+app.debug = True
 
 from app.home import home as home_blueprint
 from app.admin import admin as admin_blueprint
 
-app = Flask(__name__)
-app.debub = True
+# app = Flask(__name__)
+# app.debug = True
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+
+@app.errorhandler(404)
+def page_not_fund(error):
+    return render_template('home/404.html'), 404
