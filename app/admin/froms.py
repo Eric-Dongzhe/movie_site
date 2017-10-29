aad671065de158b6bff46327e1e1f35586eac902
chenxin.dong <chenxin.dong@datayes.com>
2017-10-29 16:37:18 +0800

    增加后端from

---
+++ app/admin/froms.py
@@ -0,0 +1,67 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
+
+from app.models import Admin
+
+
+class LoginForm(FlaskForm):
+    account = StringField(
+        label='账号',
+        validators=[
+            DataRequired('Please input id')
+        ],
+        description='账号',
+        render_kw={
+            'class': 'form-control',
+            'placeholder': 'Please input 账号',
+            'required': 'required'
+        }
+    )
+    pwd = PasswordField(
+        label='密码',
+        validators=[
+            DataRequired('Please input id')
+        ],
+        description='密码',
+        render_kw={
+            'class': 'form-control',
+            'placeholder': 'Please input password',
+            # 'required': 'required'
+        }
+    )
+    submit = SubmitField(
+        label='登录',
+        render_kw={
+            'class': 'btn btn-primary btn-block btn-flat'
+        }
+    )
+
+    def validate_account(self, field):
+        account = field.data
+        admin = Admin.query.filter_by(name=account).count()
+        if admin == 0:
+            raise ValidationError('账号不存在! ')
+
+
+class TagForm(FlaskForm):
+    name = StringField(
+        label='标签',
+        validators=[
+            DataRequired('请输入标签! ')
+        ],
+        description='标签',
+        render_kw={
+            'class': 'form-control',
+            'id': 'input_name',
+            'placeholder': "请输入标签名称！",
+            # 'required': 'required'
+        }
+    )
+    submit = SubmitField(
+        label='确认',
+        render_kw={
+            'class': 'btn btn-primary'
+        }
+    )
+
