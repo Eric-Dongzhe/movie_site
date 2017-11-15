d9a8228b1b7359b88d1e38196354e299b6eb6b3e
chenxin.dong <chenxin.dong@datayes.com>
2017-11-15 17:40:40 +0800

    app中增加sqlite配置便于单机调试

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:spider04@spider04.wmcloud-dev.com/moviesite?charset=utf8'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///moviesite.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = 'djiaofo45dfafe4das87f'
 app.config['UP_DIR'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/uploads/')
