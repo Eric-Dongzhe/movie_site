8f3d66243163a2ac67041f2abc1f3186c5e8d3b2
chenxin.dong <chenxin.dong@datayes.com>
2017-11-24 21:52:53 +0800

    修改flask的app manager

--- manage.py
+++ manage.py
@@ -1,6 +1,9 @@
 # coding:utf-8
+from flask_script import Manager
 from app import app
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
     # app.run(host="0.0.0.0", port=5000, threaded=True)
+    manager = Manager(app)
+    manager.run()
